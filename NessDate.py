8ae6d3d9b92df93e8ceb3b5503ec10e5d44e5477
Nic P <82339329+Wreck-em@users.noreply.github.com>
2022-04-29 14:47:57 -0500

    Update NessDate.py

--- NessDate.py
+++ NessDate.py
@@ -9,8 +9,19 @@ today = date.today()
 date = today.strftime("%Y-%m-%d")
 path = "[insert path]"
 
-#gets files in "scan" directory folders
+today = date.today()
+#date = today.strftime("%Y-%m-%d")
+#variables for file path
+mydate = datetime.datetime.now()
+month = mydate.strftime("%B")
+year = mydate.strftime("%Y")
+yearmonth = year + " " + month
+#date you want to append
+date = "04/09/2022"
+
+#gets files in "scan" folders
 for file in glob.glob((path+ "/*scan*/*")):
+    dir_path = os.path.dirname(os.path.realpath(file))
     if file.endswith(".csv"):
         df = pd.read_csv(file)
         df["Date"] = date
